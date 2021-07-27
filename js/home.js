c3b8bbdb59a368cf66f2d5488c3083a359a1e6c4
Faijan-Momin <faijanmomin99@gmail.com>
2021-07-27 11:29:50 +0530

    first commit

---
+++ js/home.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".swiper-container", {
+	spaceBetween: 0,
+	slidesPerView: 1,
+    autoplay:true,
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev"
+	}
+});
