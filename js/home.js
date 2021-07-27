2ceb77af2f93b74c7edcdf3db34e1818ae669e5c
Faijan-Momin <faijanmomin99@gmail.com>
2021-07-27 22:22:03 +0530

    first commit

--- js/home.js
+++ js/home.js
@@ -1,9 +1,30 @@
-var swiper = new Swiper(".swiper-container", {
+var swiper = new Swiper(".intro", {
 	spaceBetween: 0,
 	slidesPerView: 1,
-    autoplay:true,
+    autoplay:false,
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev"
 	}
+});
+
+
+
+var swiper = new Swiper('.range', {
+    grabCursor: true,
+    centeredSlides: true,
+    freeMode: true,
+    slidesPerView: 'auto',
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true
+    }
 });
